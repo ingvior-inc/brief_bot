3eabc55e66d852b3bba315fdfbce0cb16273e6ff
ingvior-inc <cxxakds@gmail.com>
2023-06-07 00:19:13 +0300

    2.2-rus fixed

--- app/general_functions.py
+++ app/general_functions.py
@@ -21,20 +21,26 @@ async def request_to_ai(system_context: str, text_to_proccess: str) -> str:
     Отправляет запрос в OpenAI с указанием системного контекста (роль GPT в
     обработке текста + сам текст).
     """
-    response = openai.ChatCompletion.create(model='gpt-3.5-turbo',
-                                            messages=[
-                                                {'role': 'system',
-                                                 'content': system_context},
-                                                {'role': 'user',
-                                                 'content': text_to_proccess}
-                                            ],
-                                            temperature=0.8)
+    try:
+        response = openai.ChatCompletion.create(model='gpt-3.5-turbo',
+                                                messages=[
+                                                    {'role': 'system',
+                                                     'content':
+                                                         system_context},
+                                                    {'role': 'user',
+                                                     'content':
+                                                         text_to_proccess}
+                                                ],
+                                                temperature=0.8)
 
-    if 'choices' in response:
-        choices = response['choices']
-        if len(choices) > 0 and 'message' in choices[0]:
-            ai_response_text = choices[0]['message']['content']
-            return ai_response_text
+        if 'choices' in response:
+            choices = response['choices']
+            if len(choices) > 0 and 'message' in choices[0]:
+                ai_response_text = choices[0]['message']['content']
+                return ai_response_text
 
-    logging.error(response)
-    return 'Что-то не так с AI :('
+        logging.error(response)
+        return 'Что-то не так с AI :('
+
+    except openai.error.RateLimitError:
+        return 'Перегружен запросами. Обратитесь позже please'
