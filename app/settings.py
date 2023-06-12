61e94ecf20a962aac84b1ec269c68a04ff718d44
ingvior-inc <cxxakds@gmail.com>
2023-06-12 23:21:34 +0300

    3.2-rus

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ cur = connect.cursor()
 MESSAGES_LIMIT = 200
 
 # Лимит на количество сообщений в памяти бота
-BOT_MESSAGES_LIMIT = 12
+BOT_MESSAGES_LIMIT = 6
 
 INCORRECT_VALUE_MESSAGE = (f'После /start укажите число N '
                            f'(от 10 до {MESSAGES_LIMIT}) , где N - '
